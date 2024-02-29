f764bd9f865819e39ca2e13be793aa893eb090fa
Elianamos <elianamos29@gmail.com>
2024-03-01 00:23:01 +0300

    use numpy for particle simulator for optimization

--- particle-simulator/benchmark.py
+++ particle-simulator/benchmark.py
@@ -4,14 +4,19 @@ from random import uniform
 from particle import Particle
 from particle_simulator import ParticleSimulator
 
-def benchmark():
+def benchmark(npart=100, method='python'):
     particles = [
         Particle(uniform(-1.0, 1.0), uniform(-1.0, 1.0), uniform(-1.0, 1.0))
-        for i in range(1000)
+        for i in range(npart)
     ]
 
     simulator = ParticleSimulator(particles)
-    simulator.evolve(0.001)
+    
+    if method == 'python':
+        simulator.evolve_python(0.1)
+    
+    elif method == 'numpy':
+        simulator.evolve_numpy(0.1)
 
 if __name__ == '__main__':
     benchmark()
--- particle-simulator/particle_simulator.py
+++ particle-simulator/particle_simulator.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 class ParticleSimulator:
     def __init__(self, particles):
         self.particles = particles
     
-    def evolve(self, dt):
+    def evolve_python(self, dt):
         timestep = 0.00001
         nsteps = int(dt/timestep)
 
@@ -12,3 +14,21 @@ class ParticleSimulator:
                 # calculate the direction
                 norm = (p.x**2 + p.y**2)**0.5
                 p.x, p.y = (p.x - t_x_ang * p.y/norm, p.y + t_x_ang * p.x/norm)
+    
+    def evolve_numpy(self, dt):
+        timestep = 0.00001
+        nsteps = int(dt/timestep)
+
+        r_i = np.array([[p.x, p.y] for p in self.particles])
+        ang_vel_i = np.array([p.ang_vel for p in self.particles])
+
+        for i in range(nsteps):
+            norm_i = np.sqrt((r_i ** 2).sum(axis=1))
+            v_i = r_i[:, [1, 0]]
+            v_i[:, 0] *= -1
+            v_i /= norm_i[:, np.newaxis]
+            d_i = timestep * ang_vel_i[:, np.newaxis] * v_i
+            r_i += d_i
+
+            for i, p in enumerate(self.particles):
+                p.x, p.y = r_i[i]
